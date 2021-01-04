405168324a8a1a6f1dc973aaf990f72908cdb1fd
bvfnbk <bvfnbk@web.de>
2021-01-04 15:17:44 +0100

    Defines application.

--- src/main/kotlin/com/github/bvfnbk/templr/api/TemplrApplication.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/api/TemplrApplication.kt
@@ -1,10 +1,13 @@
 package com.github.bvfnbk.templr.api
 
 import com.github.bvfnbk.templr.api.model.ApplicationArguments
+import java.nio.charset.Charset
 
 /**
  * @author bvfnbk
  */
 interface TemplrApplication {
+    var charset: Charset
+
     fun run(arguments: ApplicationArguments)
 }
