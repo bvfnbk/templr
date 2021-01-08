3621a64a1e28bb063115469c3f3b3eee6ed169e0
bvfnbk <bvfnbk@web.de>
2021-01-08 14:32:33 +0100

    Adds configuration method.

--- src/main/kotlin/com/github/bvfnbk/templr/api/service/TemplateService.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/api/service/TemplateService.kt
@@ -2,11 +2,19 @@ package com.github.bvfnbk.templr.api.service
 
 import java.io.File
 import java.io.OutputStreamWriter
+import java.nio.charset.Charset
 
 /**
  * @author bvfnbk
  */
 interface TemplateService {
+    /**
+     * Configures the template service to use the given charset.
+     *
+     * @param charset The [Charset] to be used; default: [Charsets.UTF_8]
+     */
+    fun configure(charset: Charset = Charsets.UTF_8)
+
     /**
      * Combine the given template and model [File] and create a new output [File].
      *
