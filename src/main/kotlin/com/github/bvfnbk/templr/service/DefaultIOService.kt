6a50793a4fb97bb58c67ef7de12e4b172e13744e
bvfnbk <bvfnbk@web.de>
2021-01-08 14:33:34 +0100

    Implements IO service (rename)

---
+++ src/main/kotlin/com/github/bvfnbk/templr/service/DefaultIOService.kt
@@ -0,0 +1,25 @@
+package com.github.bvfnbk.templr.service
+
+import com.github.bvfnbk.templr.api.service.IOService
+import java.io.*
+import java.nio.charset.Charset
+
+/**
+ * @author bvfnbk
+ */
+class DefaultIOService : IOService {
+    override fun createFileInputStream(source: File): FileInputStream = FileInputStream(source)
+    override fun createFileOutputStream(target: File): FileOutputStream = FileOutputStream(target)
+
+    override fun createInputStreamReader(source: File, charset: Charset): InputStreamReader =
+        createInputStreamReader(createFileInputStream(source), charset)
+
+    override fun createInputStreamReader(inputStream: FileInputStream, charset: Charset): InputStreamReader =
+        InputStreamReader(inputStream, charset)
+
+    override fun createOutputStreamWriter(outputStream: FileOutputStream, charset: Charset): OutputStreamWriter =
+        OutputStreamWriter(outputStream, charset)
+
+    override fun createOutputStreamWriter(target: File, charset: Charset): OutputStreamWriter =
+        createOutputStreamWriter(createFileOutputStream(target), charset)
+}
