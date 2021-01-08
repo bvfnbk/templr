44a306abd892eac57f1d61972516551b1f31e3e3
bvfnbk <bvfnbk@web.de>
2021-01-08 14:32:49 +0100

    Removes charset property.

--- src/main/kotlin/com/github/bvfnbk/templr/api/TemplrApplication.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/api/TemplrApplication.kt
@@ -1,13 +1,10 @@
 package com.github.bvfnbk.templr.api
 
 import com.github.bvfnbk.templr.api.model.ApplicationArguments
-import java.nio.charset.Charset
 
 /**
  * @author bvfnbk
  */
 interface TemplrApplication {
-    var charset: Charset
-
     fun run(arguments: ApplicationArguments)
 }
