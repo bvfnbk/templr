9723c4c6059057d870baa45c0277d3cdb2518682
bvfnbk <bvfnbk@web.de>
2021-01-08 14:33:59 +0100

    Handles charset argument.

--- src/main/kotlin/com/github/bvfnbk/templr/service/DefaultModelService.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/service/DefaultModelService.kt
@@ -12,16 +12,15 @@ import java.nio.charset.Charset
  * @author bvfnbk
  */
 class DefaultModelService(
-    private val charset: Charset,
     private val mimeService: MimeService,
     private val decoderService: DecoderService,
     private val ioService: IOService
 ) : ModelService {
-    override fun load(file: File): Map<*, *> = when (mimeService.getMimeType(file)) {
-        MimeType.JSON -> loadJson(file)
+    override fun load(file: File, charset: Charset): Map<*, *> = when (mimeService.getMimeType(file)) {
+        MimeType.JSON -> loadJson(file, charset)
     }
 
-    private fun loadJson(file: File): Map<*, *> {
+    private fun loadJson(file: File, charset: Charset): Map<*, *> {
         return decoderService.decode(ioService.createInputStreamReader(file, charset))
     }
 }
