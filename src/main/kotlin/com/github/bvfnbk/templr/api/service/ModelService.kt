2047eb38c4cf0ee8933c81c2a49a8d5298e46baf
bvfnbk <bvfnbk@web.de>
2021-01-08 14:32:18 +0100

    Adds charset argument (with default)

--- src/main/kotlin/com/github/bvfnbk/templr/api/service/ModelService.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/api/service/ModelService.kt
@@ -1,6 +1,7 @@
 package com.github.bvfnbk.templr.api.service
 
 import java.io.File
+import java.nio.charset.Charset
 
 /**
  * @author bvfnbk
@@ -8,9 +9,11 @@ import java.io.File
 interface ModelService {
     /**
      * Loads the model from the given abstract path.
+     * @param file The file from which the model is supposed to be loaded.
+     * @param charset The source character encoding; default: [Charsets.UTF_8]
      * @return the model as a [Map]
      * @throws [IllegalArgumentException] if the given [File] does not exist, is a directory rather than a regular
      * [File] or the service failed to parse the [File].
      */
-    fun load(file: File): Map<*, *>
+    fun load(file: File, charset: Charset = Charsets.UTF_8): Map<*, *>
 }
