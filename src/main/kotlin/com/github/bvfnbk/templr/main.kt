583d976af037370dd6e8ade3d583aba1e4ca8986
bvfnbk <bvfnbk@web.de>
2021-01-08 14:35:58 +0100

    switches to module and integrates koin.

--- src/main/kotlin/com/github/bvfnbk/templr/main.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/main.kt
@@ -1,45 +1,39 @@
 package com.github.bvfnbk.templr
 
-import com.github.ajalt.clikt.completion.CompletionCandidates
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.convert
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
-import com.github.ajalt.clikt.parameters.types.file
 import com.github.bvfnbk.templr.api.TemplrApplication
-import com.github.bvfnbk.templr.api.model.ApplicationArguments
-import java.nio.charset.Charset
+import com.github.bvfnbk.templr.api.service.*
+import com.github.bvfnbk.templr.freemarker.FreemarkerTemplateService
+import com.github.bvfnbk.templr.service.DefaultIOService
+import com.github.bvfnbk.templr.service.DefaultModelService
+import com.github.bvfnbk.templr.service.ExtensionBasedMimeService
+import com.github.bvfnbk.templr.service.KotlinxSerializationDecoderService
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 
-class TemplrCommand(private var application: TemplrApplication) : CliktCommand(help = "Runs the templr application.") {
-    private val charset by option(
-        "-c",
-        "--charset",
-        help = "The input/output charset to be used.",
-        metavar = "CHARSET",
-        completionCandidates = CompletionCandidates.Fixed(Charset.availableCharsets().keys)
-    ).convert {
-        Charset.forName(it)
-    }.default(Charsets.UTF_8, defaultForHelp = "UTF-8")
+val applicationModule = module {
+    // Freemarker Template Service:
+    single<TemplateService> { FreemarkerTemplateService() }
 
-    private val model by option("-m", "--model", help = "The path to the JSON model.")
-        .file()
-        .required()
+    // Services
+    single<IOService> { DefaultIOService() }
+    single<MimeService> { ExtensionBasedMimeService() }
+    single<DecoderService> { KotlinxSerializationDecoderService() }
+    single<ModelService> { DefaultModelService(get(), get(), get()) }
 
-    private val template by option("-t", "--template", help = "The path to the freemarker template.")
-        .file()
-        .required()
+    // Application:
+    single<TemplrApplication> { DefaultTemplrApplication(get(), get(), get()) }
 
-    private val output by argument().file()
-
-    override fun run() {
-        application.charset = charset
-        application.run(ApplicationArguments(model, template, output))
-    }
+    // Clikt
+    single { TemplrCommand(get()) }
 }
 
+
 fun main(args: Array<String>) {
-    TemplrCommand(DefaultTemplrApplication()).main(args)
+    startKoin {
+        modules(applicationModule)
+
+        val command = koin.get<TemplrCommand>()
+        command.main(args)
+    }
 }
