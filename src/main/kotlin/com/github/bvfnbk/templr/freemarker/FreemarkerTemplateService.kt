12bef06f94e528278a0ed35d04ce7a9a9517cff5
bvfnbk <bvfnbk@web.de>
2021-01-08 14:33:16 +0100

    Adds freemarker configuration.

--- src/main/kotlin/com/github/bvfnbk/templr/freemarker/FreemarkerTemplateService.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/freemarker/FreemarkerTemplateService.kt
@@ -3,15 +3,23 @@ package com.github.bvfnbk.templr.freemarker
 import com.github.bvfnbk.templr.api.service.TemplateService
 import freemarker.cache.FileTemplateLoader
 import freemarker.template.Configuration
+import freemarker.template.TemplateExceptionHandler
 import java.io.File
 import java.io.OutputStreamWriter
+import java.nio.charset.Charset
 
 /**
  * Encapsulates Freemarker-specifics.
  *
  * @author bvfnbk
  */
-class FreemarkerTemplateService(private val configuration: Configuration) : TemplateService {
+class FreemarkerTemplateService : TemplateService {
+    private lateinit var configuration: Configuration
+
+    init {
+        configure()
+    }
+
     override fun apply(templateFile: File, context: Map<*, *>, writer: OutputStreamWriter) {
         val (templateDirectory, templateName) = splitTemplateFile(templateFile)
 
@@ -22,6 +30,15 @@ class FreemarkerTemplateService(private val configuration: Configuration) : Temp
 
         // ...process
         template.process(context, writer)
+    }
+
+    override fun configure(charset: Charset) {
+        configuration = Configuration(Configuration.VERSION_2_3_30)
+        configuration.templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
+        configuration.defaultEncoding = charset.name()
+        configuration.logTemplateExceptions = false
+        configuration.wrapUncheckedExceptions = false
+        configuration.fallbackOnNullLoopVariable = false
     }
 
     /**
