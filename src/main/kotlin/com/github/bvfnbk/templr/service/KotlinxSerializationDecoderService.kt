9064a2ce40610fcaf5c98d396a7c89ac164a08c2
bvfnbk <bvfnbk@web.de>
2021-01-19 11:05:01 +0100

    Decode via JsonElement; mapping to generic String->Any Map.

--- src/main/kotlin/com/github/bvfnbk/templr/service/KotlinxSerializationDecoderService.kt
+++ src/main/kotlin/com/github/bvfnbk/templr/service/KotlinxSerializationDecoderService.kt
@@ -1,8 +1,8 @@
 package com.github.bvfnbk.templr.service
 
 import com.github.bvfnbk.templr.api.service.DecoderService
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.*
 
 import java.io.InputStreamReader
 
@@ -10,9 +10,21 @@ import java.io.InputStreamReader
  * @author bvfnbk
  */
 class KotlinxSerializationDecoderService : DecoderService {
-    override fun decode(reader: InputStreamReader): Map<*, *> {
-        reader.use {
-            return Json.decodeFromString(it.readText())
-        }
+    override fun decode(reader: InputStreamReader): Map<String, Any> = mapElement(decodeToJsonElement(reader))
+
+    @Suppress("UNCHECKED_CAST")
+    private fun mapElement(element: JsonElement): Map<String, Any> = when (element) {
+        is JsonObject -> decodeElement(element) as Map<String, Any>
+        else -> throw IllegalArgumentException("Given model is no JSON object.")
+    }
+
+    private fun decodeToJsonElement(reader: InputStreamReader): JsonElement = reader.use {
+        return Json.decodeFromString(it.readText())
+    }
+
+    private fun decodeElement(element: JsonElement): Any = when (element) {
+        is JsonObject -> element.entries.map { (key, value) -> Pair(key, decodeElement(value)) }.toMap()
+        is JsonArray -> element.map { listElement -> decodeElement(listElement) }
+        is JsonPrimitive -> element.content
     }
 }
